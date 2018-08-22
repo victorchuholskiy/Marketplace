b54493a1389dd486a29e82884fc1ef8b4dffbc97
viktor.chukholskiy <viktor.chukholskiy@cactussoft.biz>
2018-08-22 16:08:31 +0300

    Solve back stack bug

--- app/src/main/java/com/gmail/victorchuholskiy/marketplace/products/ProductsActivity.kt
+++ app/src/main/java/com/gmail/victorchuholskiy/marketplace/products/ProductsActivity.kt
@@ -6,6 +6,7 @@ import com.gmail.victorchuholskiy.marketplace.R
 import com.gmail.victorchuholskiy.marketplace.utils.ARG_URL
 import com.gmail.victorchuholskiy.marketplace.utils.DETAILS_FRAGMENT_TAG
 import com.gmail.victorchuholskiy.marketplace.utils.PRODUCT_FRAGMENT_TAG
+import com.moovel.android.coding.challenge.utils.replaceFragmentAddToBackStack
 import com.moovel.android.coding.challenge.utils.replaceFragmentInActivity
 import com.moovel.android.coding.challenge.utils.setupActionBar
 import dagger.Lazy
@@ -29,7 +30,7 @@ class ProductsActivity : DaggerAppCompatActivity(), ProductDetailsListener {
 
 		supportFragmentManager.findFragmentById(R.id.contentFrame)
 				as ProductsFragment? ?: productsFragmentProvider.get().also {
-			replaceFragmentInActivity(it, R.id.contentFrame, PRODUCT_FRAGMENT_TAG)
+			replaceFragmentInActivity(it, R.id.contentFrame)
 		}
 	}
 
@@ -39,7 +40,7 @@ class ProductsActivity : DaggerAppCompatActivity(), ProductDetailsListener {
 		val bundle = Bundle()
 		bundle.putString(ARG_URL, url)
 		detailsFragment.arguments = bundle
-		replaceFragmentInActivity(detailsFragment, R.id.contentFrame, DETAILS_FRAGMENT_TAG)
+		replaceFragmentAddToBackStack(detailsFragment, R.id.contentFrame, DETAILS_FRAGMENT_TAG)
 	}
 
 	override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/gmail/victorchuholskiy/marketplace/utils/Constants.kt
+++ app/src/main/java/com/gmail/victorchuholskiy/marketplace/utils/Constants.kt
@@ -5,7 +5,5 @@ package com.gmail.victorchuholskiy.marketplace.utils
  * 21/08/18.
  */
 
-const val PRODUCT_FRAGMENT_TAG = "PRODUCTS_LIST_FRAGMENT_TAG"
 const val DETAILS_FRAGMENT_TAG = "DETAILS_FRAGMENT_TAG"
-
 const val ARG_URL = "url"
